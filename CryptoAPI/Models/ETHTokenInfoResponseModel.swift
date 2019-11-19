c77f9f9be7319be3aceed7e60b407b3a7ce82556
n.fedorenko <n.fedorenko@pixelplex.io>
2019-11-19 16:02:15 +0300

    query added

---
+++ CryptoAPI/Models/ETHTokenInfoResponseModel.swift
@@ -0,0 +1,9 @@
+//
+//  ETHTokenInfoResponseModel.swift
+//  CryptoAPI
+//
+//  Created by Fedorenko Nikita on 11/18/19.
+//  Copyright © 2019 PixelPlex. All rights reserved.
+//
+
+import Foundation
