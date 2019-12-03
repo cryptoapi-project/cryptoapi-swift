0338b1b7b9fa0cce2d5a8d00ef8f9b2438933ca4
Vladimir Sharaev <v.sharaev@pixelplex.io>
2019-12-03 15:39:06 +0300

    Fixed contract address

--- CryptoAPITests/TestConstants.swift
+++ CryptoAPITests/TestConstants.swift
@@ -13,6 +13,6 @@ enum TestConstants {
     
     static let ethAddressWithBalance = "0x46Ba2677a1c982B329A81f60Cf90fBA2E8CA9fA8"
     static let ethAddressWithBalance2 = "0xCC1326778Ea3B172DDbfedD771219Ef0DB782265"
-    static let ethContractAddress = "0x9A065FdE93E3DAB6A538E9681B9946A34Ed4e023"
+    static let ethContractAddress = "0x2c33b034405b49171806698163d1f48be7c956de"
     static let transactionHash = "0xda33d43f2754d8fbd3345e933ec024dc881d5cd1c7e0219e8d6b106906223485"
 }
