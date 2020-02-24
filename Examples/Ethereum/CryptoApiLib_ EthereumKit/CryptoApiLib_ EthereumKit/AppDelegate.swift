001c13f0ac1060989e6fc8164a2a86264297fc8b
Artemy Markovsky <a.markovskiy@pixelplex.io>
2020-02-24 19:38:14 +0300

    eth examplex without comments added.

---
+++ Examples/Ethereum/CryptoApiLib_ EthereumKit/CryptoApiLib_ EthereumKit/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CryptoApiLib_ EthereumKit
+//
+//  Created by Artemy Markovsky on 2/24/20.
+//  Copyright © 2020 pixelplex. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    
+    
+}
+
---
+++ Examples/Ethereum/CryptoApiLib_web3swift/CryptoApiLib_web3swift/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  CryptoApiLib_web3swift
+//
+//  Created by Artemy Markovsky on 2/21/20.
+//  Copyright © 2020 pixelplex. All rights reserved.
+//
+
+import UIKit
+import web3swift
+import BigInt
+import CryptoApiLib
+
+class ViewController: UIViewController {
+    
+    func configCryptoApiLib() -> CryptoAPI {
+        // Initialize CryptoApi setting with your authorization token.
+        let apiSettings = Settings(authorizationToken: "Your token") { configurator in
+            configurator.networkType = NetworkType.testnet
+        }
+        let cryptoApi = CryptoAPI(settings: apiSettings)
+        
+        return cryptoApi
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cryptoApi = configCryptoApiLib()
+        
+        let mnemonicString = "your mnemonic words"
+        let toAddress = "to address"
+        
+        let keystore = try! BIP32Keystore(mnemonics: mnemonicString,
+                                          password: "",
+                                          mnemonicsPassword: "",
+                                          language: .english)!
+        let keystoreManager = KeystoreManager([keystore])
+        let web = web3(provider: InfuraProvider(.Rinkeby)!)
+        web.addKeystoreManager(keystoreManager)
+        
+        let account = try! web.wallet.getAccounts().first!
+        let address = keystore.addresses!.first!.address
+        
+        cryptoApi.eth.balance(addresses: [address]) { result in
+            switch result {
+            case .success(let addressesBalancesArray):
+                for item in addressesBalancesArray {
+                    print(item.balance)
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        let value = "10000"
+        var estimatedGas: ETHEstimateGasResponseModel?
+        cryptoApi.eth.estimateGas(fromAddress: address, toAddress: toAddress, data: "", value: value) { result in
+            switch result {
+            case .success(let response):
+                estimatedGas = response
+                print("nonse: \(response.nonce), gas prise: \(response.gasPrice), estimate: \(response.estimateGas).")
+                return
+            case .failure(let error):
+                print(error)
+                return
+            }
+        }
+        
+        guard let fee = estimatedGas else {
+            return
+        }
+        
+        guard let ethToAddress = EthereumAddress(toAddress) else {
+            print("Invalid address")
+            return
+        }
+        
+        let nonce = BigUInt(fee.nonce)
+        let gasLimit = BigUInt(fee.estimateGas)
+        guard let intTxValue = BigUInt(value), let gasPrice = BigUInt(fee.gasPrice) else {
+            return
+        }
+        let v = BigUInt(0)
+        let r = BigUInt(0)
+        let s = BigUInt(0)
+        
+        var transaction = EthereumTransaction(nonce: nonce, gasPrice: gasPrice, gasLimit: gasLimit, to: ethToAddress, value: intTxValue, data: Data(), v: v, r: r, s: s)
+        transaction.UNSAFE_setChainID(BigUInt(4)) // "4" for Rinkeby provider
+        
+        try! Web3Signer.signTX(transaction: &transaction, keystore: keystoreManager, account: account, password: "")
+        guard let transactionHash = transaction.encode()?.toHexString() else {
+            return
+        }
+        
+        cryptoApi.eth.sendRaw(transaction: transactionHash) { result in
+            switch result {
+            case .success(let response):
+                print(response.hash)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
