9964b4adf0ad0488869774e5a4809ea5e1785552
Artemy Markovsky <a.markovskiy@pixelplex.io>
2020-02-27 10:55:25 +0300

    CryptoApiLib update. Readme files added.

--- Examples/Bitcoin/CryptoApiLib_CoreBitcoin/CryptoApiLib_CoreBitcoin/ViewController.swift
+++ Examples/Bitcoin/CryptoApiLib_CoreBitcoin/CryptoApiLib_CoreBitcoin/ViewController.swift
@@ -9,13 +9,22 @@
 import UIKit
 import CryptoApiLib
 
-class ViewController: UIViewController {
+enum ExampleConstants {
+    static let authToken = "Your token"
+    
+    static let changeAddress = "sender address"
+    static let toAddress = "recipient address"
+    static let sendAmount = 2100
+    
+    static let password: String? = nil
+    static let mnemonicArray = ["array", "of", "your", "brainkey", "words"]
+}
 
+class ViewController: UIViewController {
+    
     func configCryptoApiLib() -> CryptoAPI {
-        // Or you can use custom configuration
-        let settings = Settings(authorizationToken: "Your token") { configurator in
+        let settings = Settings(authorizationToken: ExampleConstants.authToken) { configurator in
             configurator.networkType = NetworkType.testnet
-            configurator.debugEnabled = true
         }
         let api = CryptoAPI(settings: settings)
         return api
@@ -25,84 +34,80 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let cryptoApi = configCryptoApiLib()
         
-        guard let mnemonic = BTCMnemonic(words: ["array", "of", "your", "brainkey", "words"], password: nil, wordListType: .english) else {
-            print("Error: Invalid mnemonic input.")
-            return
-        }
-        guard let seed = mnemonic.words as? [String] else {
-            print("Error: Seed generating error.")
-            return
-        }
-
-        guard let keychain = mnemonic.keychain.derivedKeychain(withPath: "m/44'/1'/0'/0/0"),
-            let key = keychain.key else {
-                print("Error: Keychain generating error.")
-                return
+        let mnemonic = BTCMnemonic(words: ExampleConstants.mnemonicArray, password: ExampleConstants.password, wordListType: .english)!
+        let keychain = mnemonic.keychain.derivedKeychain(withPath: "m/44'/1'/0'/0/0")!
+        let key = keychain.key!
+        
+        // CHANGE LIMIT TO nil
+        var responseOutputs: [BTCAddressOutputResponseModel]?
+        cryptoApi.btc.addressesOutputs(addresses: [key.address.string], status: "unspent",
+                                       skip: 0, limit: 25) { result in
+                                        switch result {
+                                        case let .success(outModels):
+                                            responseOutputs = outModels
+                                            for output in outModels {
+                                                print(output.value)
+                                            }
+                                        case let .failure(error):
+                                            print(error)
+                                        }
         }
         
-        cryptoApi.btc.addressesOutputs(addresses: [key.address.string], status: "unspent", skip: 0, limit: 25) { result in
-            switch result {
-            case let .success(outModels):
-                var outputs: [__ObjC.BTCTransactionOutput] = []
-                let changeAddress = BTCAddress(string: "from Address or other")
-                let toAddress = BTCAddress(string: "to Address")
-                let value = BTCAmount(2100)
-                let fee = BTCAmount(1000)
-                
-                let tx = BTCTransaction()
-                tx.fee = fee
-                
-                var spentCoins = BTCAmount(0)
-                
-                for txOut in outputs {
-                    let txIn = BTCTransactionInput()
-                    txIn.previousHash = txOut.transactionHash
-                    txIn.previousIndex = txOut.index
-                    txIn.value = txOut.value
-                    txIn.signatureScript = txOut.script
-                    
-                    tx.addInput(txIn)
-                    spentCoins += txOut.value
-                }
-                
-                let paymentOutput = BTCTransactionOutput(value: BTCAmount(value), address: toAddress)
-                tx.addOutput(paymentOutput)
-                
-                if spentCoins > (value + fee) {
-                    let changeValue = spentCoins - (value + fee)
-                    let changeOutput = BTCTransactionOutput(value: changeValue, address: changeAddress)
-                    tx.addOutput(changeOutput)
-                }
-                
-                for i in 0..<outputs.count {
-                    let txOut = outputs[i]
-                    let txIn = tx.inputs[i] as! BTCTransactionInput
-                    
-                    guard let hash = try? tx.signatureHash(for: txOut.script, inputIndex: UInt32(i), hashType: .SIGHASH_ALL) else {
-                        return
-                    }
-                    
-                    let sigScript = BTCScript()
-                    
-                    let signature = key.signature(forHash: hash)
-                    var signatureForScript = signature
-                    let hashTypeData = BTCSignatureHashType.SIGHASH_ALL.rawValue
-                    var hashType = hashTypeData
-                    signatureForScript?.append(&hashType, count: 1)
-                    _ = sigScript?.appendData(signatureForScript)
-                    _ = sigScript?.appendData(key.publicKey as Data?)
-                    
-                    if !key.isValidSignature(signature, hash: hash) {
-                        return
-                    }
-
-                    txIn.signatureScript = sigScript
-                }
-
-            case let .failure(error):
-                print(error)
-            }
+        var outputs = mapOutputsResponse(model: responseOutputs!)
+        let changeAddress = BTCAddress(string: ExampleConstants.changeAddress)
+        let toAddress = BTCAddress(string: ExampleConstants.toAddress)
+        let value = BTCAmount(ExampleConstants.sendAmount)
+        let fee = BTCAmount(1000)
+        
+        let transaction = BTCTransaction()
+        transaction.fee = fee
+        
+        var spentCoins = BTCAmount(0)
+        
+        for txOut in outputs {
+            let txIn = BTCTransactionInput()
+            txIn.previousHash = txOut.transactionHash
+            txIn.previousIndex = txOut.index
+            txIn.value = txOut.value
+            txIn.signatureScript = txOut.script
+            
+            transaction.addInput(txIn)
+            spentCoins += txOut.value
         }
+        
+        let paymentOutput = BTCTransactionOutput(value: BTCAmount(value), address: toAddress)
+        transaction.addOutput(paymentOutput)
+        
+        if spentCoins > (value + fee) {
+            let changeValue = spentCoins - (value + fee)
+            let changeOutput = BTCTransactionOutput(value: changeValue, address: changeAddress)
+            transaction.addOutput(changeOutput)
+        }
+        
+        for i in 0..<outputs.count {
+            let txOut = outputs[i]
+            let txIn = transaction.inputs[i] as! BTCTransactionInput
+            
+            let hash = try! transaction.signatureHash(for: txOut.script, inputIndex: UInt32(i), hashType: .SIGHASH_ALL)
+            
+            let sigScript = BTCScript()
+            
+            let signature = key.signature(forHash: hash)
+            var signatureForScript = signature
+            let hashTypeData = BTCSignatureHashType.SIGHASH_ALL.rawValue
+            var hashType = hashTypeData
+            signatureForScript?.append(&hashType, count: 1)
+            _ = sigScript?.appendData(signatureForScript)
+            _ = sigScript?.appendData(key.publicKey as Data?)
+            
+            if !key.isValidSignature(signature, hash: hash) {
+                return
+            }
+            
+            txIn.signatureScript = sigScript
+        }
+        
+        let transactionHex = BTCHexFromData(transaction.data)!
     }
 }
 
